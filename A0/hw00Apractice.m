79480f47d7cb582aff8359b54cbf0354d69df1e3
Kai Sackville-Hii <sackvillehiik@gmail.com>
2019-01-08 17:55:55 -0800

    part A added

---
+++ A0/hw00Apractice.m
@@ -0,0 +1,48 @@
+%MACM 316 - MatLab Warmup Problem A 
+%Taylor Series 
+%
+%Please Refer to the the 'Matlab Warmup' assignment sheet found on Canvas.
+%
+%Purpose: The purpose of this problem is to compute a truncated
+%       | Taylor Series of the function r(t)=t^3*exp(at). We
+%       | will do this by reviewing/learning about inline
+%       | functions, for loops, and vector syntax in MatLab.  
+%       | We will also see a brief introduction to computing
+%       | errors.
+
+
+t = [-2:.05:2]; 
+% r = @(t) t.^2.*exp(t); %old r
+r = @(t) t.^3.*exp(2.*t);
+s=0;
+
+for k = 0:15
+%     s = s + 1/factorial(k)*(t).^(k+2);
+    s = s + (t.^3).*(1/factorial(k) * (2).^(k) * (t).^(k));
+end 
+
+Err=s-r(t);
+
+%Type your new plot title here
+plot_title = 'Plot of r(t) = t^2exp(t) and Taylor Approximation s(t)';
+
+%Plotting of results
+figure(1)
+clf
+hold on 
+plot(t,r(t),'b')
+plot(t,s,'r--')
+grid on 
+xlabel('t','fontsize',15)
+ylabel('y','fontsize',15)
+title(plot_title,'fontsize',15)
+legend({'r(t)','s(t)'},'fontsize',15)
+
+figure(2)
+clf
+hold on 
+plot(t,Err,'k')
+grid on 
+xlabel('t','fontsize',15)
+ylabel('Err(t)','fontsize',15)
+title('Error plot','fontsize',15)
